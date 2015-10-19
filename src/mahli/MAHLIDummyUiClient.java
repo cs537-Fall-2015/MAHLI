f89f4abb4a96c73eb7bf1facdf9c2cd2c42979ce
hsantizo <henry.santizo@gmail.com>
2015-10-19 15:10:14 -0700

    commit for MAHLI's GUI Layout

--- src/mahli/MAHLIDummyUiClient.java
+++ src/mahli/MAHLIDummyUiClient.java
@@ -2,6 +2,8 @@ package mahli;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -12,7 +14,6 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 import javax.swing.AbstractButton;
-import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -31,34 +32,52 @@ public class MAHLIDummyUiClient extends RoverClientRunnable {
 
 	@Override
 	public void run() {
-		ButtonDemo bd = new ButtonDemo();
-		bd.mainRun();
+		GUILayout gl = new GUILayout();
+		gl.mainRun();
 
 	}
 
-	public class ButtonDemo extends JPanel implements ActionListener {
+	public class GUILayout extends JPanel implements ActionListener {
+		private static final long serialVersionUID = 1L;
+		
 		protected JButton b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12;
 		protected JTextArea ta;
 		protected JScrollPane sp;
 
-		public ButtonDemo() {
+		public GUILayout() {
+			// set the window layout manager
+			setLayout(new FlowLayout());
+			// set the window location when it starts
+	        setLocation(0, 0);
 			
-			Dimension d = new Dimension(700,500);
-	    	setPreferredSize(d);
-	    	setLocation(0, 0);
-	    	setBorder(BorderFactory.createLineBorder(Color.black));
-	    	ta = new JTextArea(8,50);
-	    	ta.setLineWrap(true);
-	        ta.setWrapStyleWord(true);
-	        ta.setLocation(300, 300);
-	        sp = new JScrollPane(ta);  
-
+	        // set the dimensions for the panels
+			Dimension leftDimension = new Dimension(200,700);
+			Dimension rightDimension = new Dimension(500,700);
+			
+			// create the panel to hold the buttons and output from the server
+			JPanel buttonPanel = new JPanel(new GridLayout(15, 1, 5, 5));
+			JPanel outputPanel = new JPanel(new GridLayout(1, 1));
+	        
+			// set the preferred panel size
+	        buttonPanel.setPreferredSize(leftDimension);
+	        outputPanel.setPreferredSize(rightDimension);
+	        
+	        // set the panel layout manager
+	        buttonPanel.setLayout(new GridLayout(15, 2, 5, 5));
+	        outputPanel.setLayout(new GridLayout());
+	        
+	        // set the panel background color
+	        buttonPanel.setBackground(Color.DARK_GRAY);
+	        outputPanel.setBackground(Color.YELLOW);
+	        
+	        // set the panel to be visible
+	        buttonPanel.setVisible(true);
+	        outputPanel.setVisible(true);
+	        
+	        // set the buttons parameters
 			b1 = new JButton("MAHLI_Camera_ON");
 			b1.setVerticalTextPosition(AbstractButton.CENTER);
-			b1.setHorizontalTextPosition(AbstractButton.LEADING); // aka LEFT,
-																	// for
-																	// left-to-right
-																	// locales
+			b1.setHorizontalTextPosition(AbstractButton.LEADING); 
 			b1.setMnemonic(KeyEvent.VK_D);
 			b1.setActionCommand("MAHLI_Camera_ON");
 
@@ -68,66 +87,46 @@ public class MAHLIDummyUiClient extends RoverClientRunnable {
 			b2.setMnemonic(KeyEvent.VK_M);
 
 			b3 = new JButton("MAHLI_NightIllumination_OFF");
-			// Use the default text position of CENTER, TRAILING (RIGHT).
 			b3.setMnemonic(KeyEvent.VK_E);
 			b3.setActionCommand("MAHLI_NightIllumination_OFF");
-			// b3.setEnabled(false);
 
 			b4 = new JButton("MAHLI_AutoFocus_ON");
-			// Use the default text position of CENTER, TRAILING (RIGHT).
 			b4.setMnemonic(KeyEvent.VK_E);
 			b4.setActionCommand("MAHLI_AutoFocus_ON");
-			// b4.setEnabled(false);
 
 			b5 = new JButton("MAHLI_AutoFocus_OFF");
-			// Use the default text position of CENTER, TRAILING (RIGHT).
 			b5.setMnemonic(KeyEvent.VK_E);
 			b5.setActionCommand("MAHLI_AutoFocus_OFF");
-			// b5.setEnabled(false);
 
 			b6 = new JButton("MAHLI_Video_ON");
-			// Use the default text position of CENTER, TRAILING (RIGHT).
 			b6.setMnemonic(KeyEvent.VK_E);
 			b6.setActionCommand("MAHLI_Video_ON");
-			// b6.setEnabled(false);
 
 			b7 = new JButton("MAHLI_Video_OFF");
-			// Use the default text position of CENTER, TRAILING (RIGHT).
 			b7.setMnemonic(KeyEvent.VK_E);
 			b7.setActionCommand("MAHLI_Video_OFF");
-			// b7.setEnabled(false);
 
 			b8 = new JButton("MAHLI_Image_Capture");
-			// Use the default text position of CENTER, TRAILING (RIGHT).
 			b8.setMnemonic(KeyEvent.VK_E);
 			b8.setActionCommand("MAHLI_Image_Capture");
-			// b8.setEnabled(false);
 
 			b9 = new JButton("MAHLI_Image_store");
-			// Use the default text position of CENTER, TRAILING (RIGHT).
 			b9.setMnemonic(KeyEvent.VK_E);
 			b9.setActionCommand("MAHLI_Image_store");
-			// b9.setEnabled(false);
 
 			b10 = new JButton("MAHLI_Image_read");
-			// Use the default text position of CENTER, TRAILING (RIGHT).
 			b10.setMnemonic(KeyEvent.VK_E);
 			b10.setActionCommand("MAHLI_Image_read");
-			// b10.setEnabled(false);
 
 			b11 = new JButton("MAHLI_Camera_OFF");
-			// Use the default text position of CENTER, TRAILING (RIGHT).
 			b11.setMnemonic(KeyEvent.VK_E);
 			b11.setActionCommand("MAHLI_Camera_OFF");
-			// b11.setEnabled(false);
 
 			b12 = new JButton("EXIT");
-			// Use the default text position of CENTER, TRAILING (RIGHT).
 			b12.setMnemonic(KeyEvent.VK_E);
 			b12.setActionCommand("EXIT");
-			// b12.setEnabled(false);
 
-			// Listen for actions on buttons 1 and 3.
+			// Listen for button actions
 			b1.addActionListener(this);
 			b11.addActionListener(this);
 			b2.addActionListener(this);
@@ -140,25 +139,33 @@ public class MAHLIDummyUiClient extends RoverClientRunnable {
 			b9.addActionListener(this);
 			b10.addActionListener(this);
 			b12.addActionListener(this);
+			
+			// set the text area parameters
+			ta = new JTextArea(50, 100);
+	    	ta.setLineWrap(true);
+	        ta.setWrapStyleWord(true);
+	        sp = new JScrollPane(ta);
 
-			b1.setToolTipText("Click this button to disable the middle button.");
-			b2.setToolTipText("This middle button does nothing when you click it.");
-			b3.setToolTipText("Click this button to enable the middle button.");
-
-			// Add Components to this container, using the default FlowLayout.
-			add(b1);
-			add(b11);
-			add(b2);
-			add(b3);
-			add(b4);
-			add(b5);
-			add(b6);
-			add(b7);
-			add(b8);
-			add(b9);
-			add(b10);
-			add(b12);
-			add(sp);
+			// add all the buttons to the left panel
+	        buttonPanel.add(b1);
+	        buttonPanel.add(b11);
+	        buttonPanel.add(b2);
+	        buttonPanel.add(b3);
+	        buttonPanel.add(b4);
+	        buttonPanel.add(b5);
+	        buttonPanel.add(b6);
+	        buttonPanel.add(b7);
+	        buttonPanel.add(b8);
+	        buttonPanel.add(b9);
+	        buttonPanel.add(b10);
+	        buttonPanel.add(b12);
+	        
+	        // add the text area to the right panel
+	        outputPanel.add(sp);
+	        
+	        // add the panel to the main GUI panel
+	        add(buttonPanel);
+	        add(outputPanel);
 		}
 
 		public void actionPerformed(ActionEvent e) {
@@ -169,7 +176,6 @@ public class MAHLIDummyUiClient extends RoverClientRunnable {
 			String reply = "";
 
 			command = e.getActionCommand();
-			//System.out.println(command);
 
 			try {
 				outputToAnotherObject = new ObjectOutputStream(getRoverSocket().getNewSocket().getOutputStream());
@@ -179,8 +185,7 @@ public class MAHLIDummyUiClient extends RoverClientRunnable {
 			} catch (ClassNotFoundException | IOException e1) {
 				e1.printStackTrace();
 			}
-			//System.out.println("MAHLI CLIENT : Response from server - "+reply);
-			ta.append("MAHLI CLIENT : Response from server - "+reply);
+			ta.append("MAHLI CLIENT : Response from server - " + reply);
 			ta.append("\n");
 			
 			if(reply.equalsIgnoreCase("exit"))
@@ -190,7 +195,7 @@ public class MAHLIDummyUiClient extends RoverClientRunnable {
 
 		/** Returns an ImageIcon, or null if the path was invalid. */
 		protected  ImageIcon createImageIcon(String path) {
-			java.net.URL imgURL = ButtonDemo.class.getResource(path);
+			java.net.URL imgURL = GUILayout.class.getResource(path);
 			if (imgURL != null) {
 				return new ImageIcon(imgURL);
 			} else {
@@ -210,7 +215,7 @@ public class MAHLIDummyUiClient extends RoverClientRunnable {
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 			// Create and set up the content pane.
-			ButtonDemo newContentPane = new ButtonDemo();
+			GUILayout newContentPane = new GUILayout();
 			newContentPane.setOpaque(true); // content panes must be opaque
 			frame.setContentPane(newContentPane);
 
