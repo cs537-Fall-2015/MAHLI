257c0c37b369e688c3a1248950ddf28d924d4d4f
hsantizo <henry.santizo@gmail.com>
2015-10-24 15:10:40 -0700

    commit 1 file MasterMain to branch henry

--- src/main/MasterMain.java
+++ src/main/MasterMain.java
@@ -13,7 +13,7 @@ public class MasterMain {
 	public static void main(String[] args) {
 		//Each module has its own port
 		int port_mahli = 9010;
-		int port_power = 9053; // changed port number
+		int port_power = 9013;
 		
 		try {
 			// create a thread for module one
