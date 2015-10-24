b3011fa2272e70eec61757880ef8ab7b9ed030d6
kunaljangade <jangade.kunal@gmail.com>
2015-10-24 14:58:59 -0700

    mahliServer change

--- src/module1/MahliServer.java
+++ src/module1/MahliServer.java
@@ -1,4 +1,4 @@
-package module2;
+package module1;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -6,18 +6,19 @@ import java.io.ObjectOutputStream;
 
 import generic.RoverServerRunnable;
 
-public class ModuleTwoServer extends RoverServerRunnable {
+public class MahliServer extends RoverServerRunnable {
 
-	public ModuleTwoServer(int port) throws IOException {
+	public MahliServer(int port) throws IOException {
 		super(port);
 	}
 
 	@Override
 	public void run() {
+
 		try {
 			while (true) {
 				
-				System.out.println("Module 2 Server: Waiting for client request");
+				System.out.println("Mahli Server: Waiting for client request");
 				
 				// creating socket and waiting for client connection
 				getRoverServerSocket().openSocket();
@@ -27,24 +28,24 @@ public class ModuleTwoServer extends RoverServerRunnable {
 				
 				// convert ObjectInputStream object to String
 				String message = (String) inputFromAnotherObject.readObject();
-				System.out.println("Module 2 Server: Message Received from Client - "+ message.toUpperCase());
+				System.out.println("Mahli Server: Message Received from Client - "+ message.toUpperCase());
 				
 				// create ObjectOutputStream object
-				ObjectOutputStream outputToAnotherObject = new ObjectOutputStream(getRoverServerSocket().getSocket().getOutputStream());
+				//ObjectOutputStream outputToAnotherObject = new ObjectOutputStream(getRoverServerSocket().getSocket().getOutputStream());
 				
 				// write object to Socket
-				outputToAnotherObject.writeObject("Module 2 Server response Hi Client - " + message);
+				//outputToAnotherObject.writeObject("Module 1 Server response Hi Client - " + message);
 				
 				// close resources
 				inputFromAnotherObject.close();
-				outputToAnotherObject.close();
+				//outputToAnotherObject.close();
 				
 				// getRoverServerSocket().closeSocket();
 				// terminate the server if client sends exit request
 				if (message.equalsIgnoreCase("exit"))
 					break;
 			}
-			System.out.println("Server: Shutting down Socket server 2!!");
+			System.out.println("Server: Shutting down Socket server 1!!");
 			// close the ServerSocket object
 			closeAll();
 		} catch (IOException e) {
