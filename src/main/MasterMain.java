cdcdee89327c67700334e430f01ef5970d27d25f
Parthvi111 <parthvi111@gmail.com>
2015-10-31 15:16:15 -0700

    11

--- src/main/MasterMain.java
+++ src/main/MasterMain.java
@@ -12,7 +12,7 @@ public class MasterMain {
 
 	public static void main(String[] args) {
 		//Each module has its own port
-		int port_mahli = 9010; //assign ports
+		int port_mahli = 9010; //assign ports11
 		int port_power = 9013;
 		
 		try {
