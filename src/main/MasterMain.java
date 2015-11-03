fdc82dabd697634f83c23a4230fba1a826dd5792
hsantizo <henry.santizo@gmail.com>
2015-11-03 14:29:42 -0800

    commit deleted module two, usecase, and gson jar library. removed gson imports on WriteToJSON.

--- src/main/MasterMain.java
+++ src/main/MasterMain.java
@@ -12,15 +12,15 @@ public class MasterMain {
 
 	public static void main(String[] args) {
 		//Each module has its own port
-		int port_mahli = 9010; //assigned port
-		int port_power = 9013; // assigned port
+		int port_mahli = 9010;
+		int port_power = 9013;
 		
 		try {
 			// create a thread for module one
 			ModuleOneServer serverOne = new ModuleOneServer(port_power);
 			Thread server_1 = RoverThreadHandler.getRoverThreadHandler().getNewThread(serverOne);
 						
-			//create a thread for module MAHLIIS
+			//create a thread for module MAHLI
 			MAHLIServer serverMahli = new MAHLIServer(port_mahli);
 			Thread server_3 = RoverThreadHandler.getRoverThreadHandler().getNewThread(serverMahli);
 						
